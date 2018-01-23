fc85611ea1818f84b94fb91638db4a8d37a1dd2f
geobe <code@georgbeier.de>
2018-01-23 17:32:47 +0100

    completed Groovy state machine

--- src/main/groovy/de/geobe/util/statemachine/StateMachine.groovy
+++ src/main/groovy/de/geobe/util/statemachine/StateMachine.groovy
@@ -31,39 +31,32 @@ import groovy.util.logging.Slf4j
  * <ul><li>States as enums,</li>
  * <li>Events as enums,</li>
  * <li>Transitions are triggered by a combination of current state and event,</li>
- * <li>onExit actions executed when leaving a state,</li>
- * <li>internalActions triggered by an event without state change.</li>
- * <li>onEntry actions executed when entering a state,</li>
- * <li>transitions actions executing after leaving a state and before entering a following state.
+ * <li>they can be guarded by a Closure inhibiting their execution,</li>
+ * <li>onExit activities executed when leaving a state,</li>
+ * <li>internal activities triggered by an event without state change.</li>
+ * <li>onEntry activities executed when entering a state,</li>
+ * <li>transitions activities executing after leaving a state and before entering a following state.
  * They can return a target state changing the following state.</li></ul>
  * @author georg beier
- * @param S enumertion Type for States
+ * @param S enumertion Type for States. Limited by current implementation to 4095 values.
  * @param E enumeration type for events
  */
 @Slf4j
 class StateMachine<S extends Enum, E extends Enum> {
     /** map for internal actions, indexed by combination of currentState and event */
-    private Map<Integer, SmAction> stateMachine = new HashMap<>()
+    private Map<Integer, Closure> stateMachine = new HashMap<>()
     /** map for next states, indexed by combination of currentState and event */
     private Map<Integer, S> nextState = new HashMap<>()
     /** map of closures as entry actions */
-    private Map<S, SmAction> onEntry = new HashMap<>()
+    private Map<S, Closure> onEntry = new HashMap<>()
     /** map of closures as exit actions */
-    private Map<S, SmAction> onExit = new HashMap<>()
-    /** Actions on a transition */
-    private Map<TxKey, SmAction> transitions = new HashMap<>()
-    /** index map of SxEs */
-    private Map<Integer, TxKey> txMap = new HashMap<>()
+    private Map<S, Closure> onExit = new HashMap<>()
+    /** Map of all transitions with guards and actions */
+    private Map<Integer, List<TxDef<S>>> transitions = new HashMap<>()
     /** store current state */
     private S currentState
     /** for logging info to identify state machine instance */
     private String smid
-
-    public Map<S, SmAction> getOnEntry() { onEntry }
-
-    public Map<S, SmAction> getOnExit() { onExit }
-
-    public HashMap<TxKey, SmAction> getTransitions() { transitions }
 
     def getCurrentState() { currentState }
 
@@ -79,14 +72,76 @@ class StateMachine<S extends Enum, E extends Enum> {
         smid = id
     }
 
-    private buildIndex() {
-        transitions.keySet().each { TxKey key ->
-            txMap[key.trix()] = key
-        }
+    /**
+     * Add a transition to the state machine linking two states. It is not guarded, i.e. its guard is always true.
+     * @param from the state, from where transition starts
+     * @param to the state, where transition leads to. May be same as from state.
+     * @param ev the event that triggers this transition
+     * @param action (optional, default is do nothing) Closure that is executed during the transition. It may return
+     * a different target state that overwrites to parameter. Thus branching transitions can be easily implemented.
+     * Closure may have parameters of type Object...
+     */
+    void addTransition(S from, S to, E ev, Closure action = {}) {
+        addGuardedTransition(from, to, ev, { true }, action)
     }
 
     /**
-     * execute Action that is identified by currentState and event.
+     * Add an "internal" transition that does not leave its state but executes some activity.
+     * @param inState State in which this transition may happen
+     * @param ev the event that triggers this transition
+     * @param action Closure that is executed during the transition. Closure may have parameters
+     * of type Object... .
+     * @param guard (optional, default true) Closure that controls execution of the activity.
+     * Only when it returns (Groovy-) true, activity is executed
+     */
+    void addInternalActivity(S inState, E ev, Closure action, Closure guard = { true }) {
+        addGuardedTransition(inState, null, ev, guard, action)
+    }
+
+    /**
+     * Add a transition to the state machine linking two states
+     * @param from the state, from where transition starts
+     * @param to the state, where transition leads to. May be same as from state.
+     * @param ev the event that triggers this transition
+     * @param guard Closure that controls execution of the transition. Only when it returns (Groovy-) true,
+     * transition is executed
+     * @param action Closure that is executed during the transition. It may return a different target state that
+     * overwrites to parameter. Thus branching transitions can be easily implemented. Closure may have parameters
+     * of type Object... .
+     */
+    void addGuardedTransition(S from, S to, E ev, Closure guard, Closure action = {}) {
+        def index = trix(from, ev)
+        if (!transitions[index]) {
+            transitions[index] = []
+        }
+        TxDef<S> tx = new TxDef<>([start: from, target: to, action: action, guard: guard])
+        transitions[index].add tx
+    }
+
+    /**
+     * Add an onEtry activity to the state machine that is executed each time a transition enters
+     * the state, even if it comes from the same state.
+     * @param state which gets the activiy. Only one entry activity per state is supported
+     * @param action Closure that implements the activity. Must not have parameters.
+     */
+    void addEntryAction(S state, Closure action) {
+        onEntry[state] = action
+    }
+
+    /**
+     * Add an onExit activity to the state machine that is executed each time a transition leaves
+     * the state, even if it leads back to the same state.
+     * @param state which gets the activiy. Only one exit activity per state is supported
+     * @param action Closure that implements the activity. Must not have parameters.
+     */
+    void addExitAction(S state, Closure action) {
+        onExit[state] = action
+    }
+
+    /**
+     * Actions are identified by currentState and incoming event.
+     * Execute first matching action with a guard delivering (Groovy-) true.
+     * If none is found, no action is executed and state stays unchanged.
      * After execution, statemachine will be
      * <ul><li>in the following state as defined in addTransition method,
      * if closure returns no object of type S</li>
@@ -100,74 +155,57 @@ class StateMachine<S extends Enum, E extends Enum> {
      */
     S execute(E event, Object... params) {
         def index = trix(currentState, event)
-        if (!txMap)
-            buildIndex()
-        TxKey txKey = txMap[index]
-        if (txKey) {
-            if(txKey.target) {
+        if (transitions[index]) {
+//            S fromState = currentState
+            List<TxDef<S>> txlist = transitions[index]
+            // find first with guard evaluating to true
+            TxDef<S> tx = txlist.find {
+                it?.guard() ? true : false
+            }
+            if (tx?.target) {
                 // full transition
-                def clex = onExit[txKey.start]
-                clex?.call()
-                def clac = transitions[txKey]
-                def result = clac?.call(params)
+                onExit[tx.start]?.call()
+                def result = tx.action?.call(params)
                 def next
                 if (result instanceof S) {
                     next = result
                 } else {
-                    next = txKey.target
+                    next = tx.target
                 }
-                def clen = onEntry[next]
-                clen?.call()
-                log.info("Transition $smid: $currentState--$event->$next")
                 currentState = next
+                onEntry[next]?.call()
+//                log.debug("Transition $smid: $currentState--$event->$next")
             } else {
                 // inner transition
-                def clac = transitions[txKey]
-                clac?.call()
-                log.info("Inner Transition $smid: $currentState--$event->$currentState")
+                tx?.action?.call(params)
+//                log.debug("Inner Transition $smid: $fromState--$event->$currentState")
             }
         } else {
-            log.info("ignored event $event in fromState $currentState")
+            log.warn("ignored event $event in fromState $currentState")
         }
         currentState
     }
 
     /**
-     * calculate a unique transition trix from current state and triggering event
+     * calculate a unique transition index from current state and triggering event
      * @param st current state
      * @param ev event triggering transition
      * @return a unique Integer computed from state and event
      */
-    public static Integer trix(S st, E ev) {
+    private static Integer trix(S st, E ev) {
         def t = st.ordinal() + (ev.ordinal() << 12)
         t
     }
 
     /**
-     * This SAM type (a type which defines a single abstract method) is used to
-     * define state machine actions. It can be implemented directly by a Closure.
+     * supporting data structure to store transition information
+     * @param <S> the state enumeration type
      */
-    interface SmAction {
-        S act(Object... params)
-    }
-
-    static class TxKey<S, E> implements Comparable<TxKey> {
+    private static class TxDef<S> {
         S start
         S target
-        E trigger
-
-        TxKey(S st, S tg, E tr) {
-            start = st
-            target = tg
-            trigger = tr
-        }
-
-        @Override
-        int compareTo(TxKey o) {
-            return trix(start, trigger).compareTo(trix(o.start, o.trigger))
-        }
-
-        def trix() { trix(start, trigger) }
+        Closure guard = {}
+        Closure action = {}
     }
 }
 
--- src/test/groovy/de/geobe/util/statemachine/StateMachineSpecification.groovy
+++ src/test/groovy/de/geobe/util/statemachine/StateMachineSpecification.groovy
@@ -35,90 +35,170 @@ import spock.lang.Specification
 class StateMachineSpecification extends Specification {
 
     static enum State {
-        START, S1, S2, S3, END
+        START, S1, S2, S3, SG, END
     }
 
     static enum Event {
-        Start, Intern, E1, E2, Exit
+        Start, Intern, E1, E2, EG, Exit
     }
 
     StateMachine<State, Event> sm
+    StringBuffer sbuf
+
+    def enter = {
+        sbuf.append(" enter ${sm.currentState}\n")
+    }
+    def exit = { sbuf.append("leave ${sm.currentState} ") }
 
     def setup() {
+        sbuf = new StringBuffer()
         sm = new StateMachine<>(State.START, 'smtest')
-        def enter = {
-            log.info("enter ${sm.currentState}")
-        }
-        def exit = { log.info("leaving ${sm.currentState}") }
-        def transarg = {Object... args -> log.info "on Transition with args: ${args[0]}"}
-        def trans = { log.info "transitioning from ${sm.currentState}" }
-        def transchange = {log.info 'transitioning to S3'; State.S3}
-        def transintern = {log.info 'internal transition'}
-
-        sm.onEntry[State.S1] = enter
-        sm.onEntry[State.S2] = enter
-        sm.onEntry[State.S3] = enter
-        sm.onEntry[State.END] = enter
-
-        def action = {log.info('Starting')}
-        sm.transitions[new StateMachine<State, Event>.TxKey(State.START, State.S1, Event.Start)] = action
-        sm.transitions[new StateMachine.TxKey(State.S1, State.S2, Event.E1)] = trans
-        sm.transitions[new StateMachine.TxKey(State.S2, State.S1, Event.E1)] = trans
-        sm.transitions[new StateMachine.TxKey(State.S1, null, Event.Intern)] = transintern
-        sm.transitions[new StateMachine.TxKey(State.S1, State.S2, Event.E2)] = transarg
-        sm.transitions[new StateMachine.TxKey(State.S2, State.S1, Event.E2)] = transchange
-        sm.transitions[new StateMachine.TxKey(State.S3, State.S3, Event.E1)] = {log.info 'looping'}
-        sm.transitions[new StateMachine.TxKey(State.S3, State.S3, Event.E2)] = {log.info 'ending'; State.END}
-        sm.transitions[new StateMachine.TxKey(State.END, State.S3, Event.E2)] = {...args ->
-            log.info 'let\'s restart'; args[0]
+        def transarg = { Object... args -> sbuf.append "--(${args[0]})->" }
+        def trans = { sbuf.append "-->" }
+        def transchange = { sbuf.append '-->'; State.S3 }
+        def transintern = { Object... args ->
+            sbuf.append "${sm.currentState} >-${args ? '(' + args + ')' : ''}-<\n"
         }
 
-        sm.onExit[State.S1] = exit
-        sm.onExit[State.S2] = exit
-        sm.onExit[State.S3] = exit
-        sm.onExit[State.START] = exit
+        sm.addEntryAction(State.S1, enter)
+        sm.addEntryAction(State.S2, enter)
+        sm.addEntryAction(State.S3, enter)
+        sm.addEntryAction(State.END, enter)
+
+        def action = { sbuf.append('-Starting->') }
+        sm.addTransition(State.START, State.S1, Event.Start, action)
+        sm.addTransition(State.S1, State.S2, Event.E1, trans)
+        sm.addTransition(State.S2, State.S1, Event.E1, trans)
+        sm.addInternalActivity(State.S1, Event.Intern, transintern)
+        sm.addTransition(State.S1, State.S2, Event.E2, transarg)
+        sm.addTransition(State.S2, State.S1, Event.E2, transchange)
+        sm.addTransition(State.S3, State.S3, Event.E1, { sbuf.append '--loop->' })
+        sm.addTransition(State.S3, State.S3, Event.E2, { sbuf.append '--end->'; State.END })
+        sm.addTransition(State.END, State.S3, Event.E2, { ... args ->
+            sbuf.append 'goto restart \n\n'; args[0]
+        })
+
+        sm.addExitAction(State.S1, exit)
+        sm.addExitAction(State.S2, exit)
+        sm.addExitAction(State.S3, exit)
+        sm.addExitAction(State.START, { sbuf.append('(O)--') })
 
     }
 
-    def 'the state machine should perform nicely' () {
+    def cleanup() {
+        log.info("\n${sbuf.toString()}")
+    }
+
+    def 'do nothing but initialize tests'() {
+        when: 'we are here'
+        then: 'sm exists'
+        sm.currentState == State.START
+    }
+
+    def 'the sm should respect guarded transitions'() {
+        given: 'some new guarded transitions'
+        boolean guard
+        int count = 0
+        sm.addEntryAction(State.SG, enter)
+        sm.addExitAction(State.SG, exit)
+        sm.addGuardedTransition(State.S1, State.SG, Event.EG, { 42 })
+        sm.addGuardedTransition(State.S2, State.SG, Event.EG, { true })
+        sm.addGuardedTransition(State.SG, State.S1, Event.EG, { guard })
+        sm.addGuardedTransition(State.SG, State.S2, Event.EG, { !guard })
+        sm.addInternalActivity(State.SG, Event.Intern, { Object... args ->
+            sbuf.append "${sm.currentState} >-${args ? '(' + args + ')' : ''}-<\n"
+            count += args[0]
+        }, { guard })
+        when: 'sm goes START -Start-> S1 -EG{42}->'
+        sm.execute(Event.Start)
+        sm.execute(Event.EG)
+        then: 'sm is in SG'
+        sm.currentState == State.SG
+        when: 'guard is true, --EG->'
+        guard = true
+        sm.execute(Event.EG)
+        then: 'sm is in S1'
+        sm.currentState == State.S1
+        when: 'guard is false, --EG->'
+        guard = false
+        sm.execute(Event.EG)
+        sm.execute(Event.EG)
+        then: 'sm is in S2'
+        sm.currentState == State.S2
+        when: 'guard is true -Intern->'
+        guard = true
+        sm.execute(Event.EG)
+        sm.execute(Event.Intern, 42)
+        then: 'internal event/action is executed'
+        count == 42
+        sm.currentState == State.SG
+        when: 'guard is not true '
+        guard = false
+        count = 0
+        sm.execute(Event.Intern, 42)
+        then: 'internal event/action is not executed'
+        count == 0
+        sm.currentState == State.SG
+    }
+
+//    @Ignore
+    def 'the state machine should perform nicely'() {
         when: 'sm is created'
         then: 'it should be in Stert state'
-        assert sm.currentState == State.START
+        sm.currentState == State.START
         when: 'sm is started'
         sm.execute(Event.Start)
         then: 'sm is in S1'
-        assert sm.currentState == State.S1
+        sm.currentState == State.S1
         when: 'E1 in S1'
         sm.execute(Event.E1)
         then: 'sm in S2'
-        assert sm.currentState == State.S2
+        sm.currentState == State.S2
         when: 'E1 in S2'
         sm.execute(Event.E1)
         then: 'sm in S1'
-        assert sm.currentState == State.S1
+        sm.currentState == State.S1
         when: 'Intern in S1'
-        sm.execute(Event.Intern)
+        sm.execute(Event.Intern, 'Hallo Intern')
         then: 'sm in S1'
-        assert sm.currentState == State.S1
+        sm.currentState == State.S1
         when: 'E2 with args in S1'
         sm.execute(Event.E2, 'Hello args', 'and much more...')
         then: 'sm in S2'
-        assert sm.currentState == State.S2
+        sm.currentState == State.S2
         when: 'E2 in S2 with change'
         sm.execute(Event.E2)
         then: 'sm in S3'
-        assert sm.currentState == State.S3
+        sm.currentState == State.S3
         when: 'E1 in S3 looping'
         sm.execute(Event.E1)
         then: 'sm in S3'
-        assert sm.currentState == State.S3
+        sm.currentState == State.S3
         when: 'E2 in S3 with external change'
         sm.execute(Event.E2)
         then: 'sm in END'
-        assert sm.currentState == State.END
+        sm.currentState == State.END
         when: 'E2 in END with restart arg'
         sm.execute(Event.E2, State.START)
         then: 'sm in START'
-        assert sm.currentState == State.START
+        sm.currentState == State.START
+    }
+
+//    @Ignore
+    def 'the sm should perform efficiently'() {
+        when: 'the sm performs a loop'
+        (0..5).each {
+            sm.execute(Event.Start)
+            sm.execute(Event.E1)
+            sm.execute(Event.E1)
+            sm.execute(Event.Intern)
+            sm.execute(Event.E2, 'Hello args', 'and much more...')
+            sm.execute(Event.E2)
+            sm.execute(Event.E1)
+            sm.execute(Event.E2)
+            sm.execute(Event.E2, State.START)
+        }
+        then: 'sm in START'
+        sm.currentState == State.START
     }
 }
